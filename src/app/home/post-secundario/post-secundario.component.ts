d998f660cf9f878fbc0dfe8b97a84d18d82d7ea3
Calixto Da Silva Neto <jcalixto@indracompany.com>
2020-02-13 13:58:14 -0300

    separando os componentes

---
+++ src/app/home/post-secundario/post-secundario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-secundario',
+  templateUrl: './post-secundario.component.html'
+})
+export class PostSecundarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
