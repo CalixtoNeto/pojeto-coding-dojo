a82f0cd7701fffb792b014623de911e3dfe77cad
Calixto Da Silva Neto <jcalixto@indracompany.com>
2020-02-13 14:58:41 -0300

    chamdo a model no componente

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
+
+  post: Post = new Post();
 
   constructor() { }
 
